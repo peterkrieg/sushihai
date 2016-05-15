d617ae6d3c4ea359c4c70eb32c95c7c7c102c420
Peter Krieg <pkrieg2@gmail.com>
2016-05-14 18:36:51 -0600

    small tweaks to main navbar, margins of links and menu closes when you click on menu item

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -18,6 +18,12 @@ angular.module('myApp')
 
 
 
+// Closes the Responsive Menu on Menu Item Click
+	$('.navbar-collapse ul li a').click(function() {
+		$('.navbar-toggle:visible').click();
+	});
+
+
 
 	// $http({
 	// 	method: 'JSONP',
